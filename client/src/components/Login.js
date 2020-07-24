7fce799f3d8ea223efb0968d0ad0a9c2ec802772
shanon <13fritsh@gmail.com>
2020-07-24 10:43:48 -0500

    added my state and event handlers for the login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+
+const initialValues = {
+  username: '',
+  password: ''
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] = useState(initialValues);
+  const {push} = useHistory();
+
+  const handleChange = event =>{
+    setLogin({
+      ...login,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const onSubmit = event =>{
+    event.preventDefault();
+
+    axios.post('http://localhost:5000/api/login', login)
+      .then(response =>{
+        localStorage.setItem('token', response.data.payload);
+      })
+      .catch(error =>{
+        console.log(error);
+      })
+      .finally(() =>{
+        setLogin(initialValues);
+      });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
